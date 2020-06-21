e443a7c1fd3d9f7d9f2401e61b5e0548df57ee14
yaowenqiang <yaowenqiang111@163.com>
2020-06-21 12:10:09 +0000

    add read demo

--- getopts.sh
+++ getopts.sh
@@ -1,9 +1,16 @@
-while getopts ':c:d:' opt
+while getopts 'cd' opt
 do
+  while (( "$#" )) ; do
+    if ! [[ $! =~ ^- ]]; then
+      username="$1"
+    fi
+    shift
+  done
+
   case "$opt" in
-    c) sudo useradd -m "$OPTARG"
+    c) sudo useradd -m "$username"
       break ;;
-    d) sudo userdel -r  "$OPTARG"
+    d) sudo userdel -r  "$username"
       break ;;
     *) echo "Usage: $0[-c|-d] <user>" ;;
   esac
